24319a169768cfa20b77b32a2c95cc4e63549f9e
iamanandhuam <anandhuanandanangmail.com>
2025-03-24 00:51:47 +0530

    Dashbord and navigation bar changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'diaplay_data_from_sw.dart';
 import 'user_input_page.dart';
-import 'landing_page.dart';
-import 'loading_indicator.dart';
 
 var logger = Logger();
 
@@ -22,46 +21,92 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Startingpage(),
-        '/landing_page': (context) => const HomePage(),
-        '/second': (context) => const SecondPage(),
-      },
+      home: const SplashScreen(),
     );
   }
 }
 
-class Startingpage extends StatelessWidget {
-  const Startingpage({super.key});
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MainScreen()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  "HEALTH SYNC",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w900,
-                    color: const Color.fromRGBO(160, 103, 234, 1),
-                    fontSize: 30,
-                  ),
-                ),
-              ],
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "HEALTH SYNC",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                color: Color.fromRGBO(160, 103, 234, 1),
+                fontSize: 30,
+              ),
             ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(), // Loading indicator
+            //const DotsLoading(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = [
+    HomePage(),
+    SecondPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.watch_rounded),
+            label: 'Healh Info',
           ),
-          Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 200.0),
-              child: DotsLoading(),
-            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.input),
+            label: 'Input',
           ),
         ],
       ),
